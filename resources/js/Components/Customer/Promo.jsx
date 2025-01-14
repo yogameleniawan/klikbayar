8a5ea323c0a86f59a8a315c863f776f88e7f234a
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2025-01-14 13:17:16 +0700

    chore: promo

--- resources/js/Components/Customer/Promo.jsx
+++ resources/js/Components/Customer/Promo.jsx
@@ -6,42 +6,48 @@ import { Image } from '@nextui-org/react';
 import "swiper/css";
 import 'swiper/css/effect-cards';
 
+const items = [
+    {
+        "slug": "pubg",
+        "image": "https://cdn.topupkuy.id/category/5e7e78a7-674e-4b81-b68b-f09ff0880555.webp"
+    },
+    {
+        "slug": "mobile-legend",
+        "image": "https://cdn.topupkuy.id/category/b7dad5cf-f304-40ca-b139-db6eb4f79654.webp"
+    },
+    {
+        "slug": "pubg",
+        "image": "https://cdn.topupkuy.id/category/5e7e78a7-674e-4b81-b68b-f09ff0880555.webp"
+    },
+]
+
 const Promo = () => {
-  return (
-    <div className="flex flex-col items-center gap-2">
-                    <h3 className="text-4xl font-semibold">Promo</h3>
-                    <span className="text-center text-sm">
-                        Geser lalu pilih dan nikmati promonya!
-                    </span>
-                    <Swiper
-                        effect={'cards'}
-                        grabCursor={true}
-                        modules={[EffectCards]}
-                        className="w-44 h-50 sm:w-52 sm:h-full mt-2"
-                    >
-                        <SwiperSlide>
-                            <Link href={route('users.index')}>
-                                <Image src="https://cdn.topupkuy.id/category/b7dad5cf-f304-40ca-b139-db6eb4f79654.webp" />
-                            </Link>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <Link href={route('users.index')}>
-                                <Image src="https://cdn.topupkuy.id/category/5e7e78a7-674e-4b81-b68b-f09ff0880555.webp" />
-                            </Link>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <Link href={route('users.index')}>
-                                <Image src="https://cdn.topupkuy.id/category/5e7e78a7-674e-4b81-b68b-f09ff0880555.webp" />
-                            </Link>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <Link href={route('users.index')}>
-                                <Image src="https://cdn.topupkuy.id/category/5e7e78a7-674e-4b81-b68b-f09ff0880555.webp" />
-                            </Link>
-                        </SwiperSlide>
-                    </Swiper>
-                </div>
-  )
+    return (
+        <div className="flex flex-col items-center gap-2">
+            <h3 className="text-4xl font-semibold">Promo</h3>
+            <span className="text-center text-sm">
+                Geser lalu pilih dan nikmati promonya!
+            </span>
+            <Swiper
+                effect={'cards'}
+                grabCursor={true}
+                modules={[EffectCards]}
+                className="w-44 h-50 sm:w-52 sm:h-full mt-2"
+            >
+                {
+                    items.map((item, i) => {
+                        return (
+                            <SwiperSlide>
+                                <Link href={route('customer.detail', { slug: item.slug })}>
+                                    <Image src={item.image} />
+                                </Link>
+                            </SwiperSlide>
+                        )
+                    })
+                }
+            </Swiper>
+        </div>
+    )
 }
 
 export default Promo

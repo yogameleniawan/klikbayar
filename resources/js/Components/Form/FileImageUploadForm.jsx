dfc1ece14f9ceb6bfd39f443ec26a2bd2497fa3a
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2025-01-15 10:54:20 +0700

    style: change text input file

--- resources/js/Components/Form/FileImageUploadForm.jsx
+++ resources/js/Components/Form/FileImageUploadForm.jsx
@@ -92,7 +92,7 @@ const FileImageUploadForm = ({
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
                             </svg>
                             <p className="mb-2 text-sm text-gray-500">
-                                <span className="font-semibold">Click to upload</span> or drag and drop
+                                <span className="font-semibold">Click to upload</span>
                             </p>
                             <p className="text-xs text-gray-500">
                                 {multiple && `Multiple files allowed`}
